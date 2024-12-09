749f6079d378c092e28604e932c368365ec82e77
Warren Parad <5056218+wparad@users.noreply.github.com>
2024-12-09 09:00:58 +0800

    Update docusaurus.config.ts fixes pages

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -37,7 +37,7 @@ const config: Config = {
       {
         pages: {
           path: '/src/pages',
-          routeBasePath: '/'
+          // routeBasePath: '/'
         },
         docs: {
           sidebarPath: './sidebars.ts',
