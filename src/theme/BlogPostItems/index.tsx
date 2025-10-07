b714c471e4c2b7b6fba600909c9325cf245bb105
Warren Parad <gpg@warrenparad.net>
2025-10-07 19:29:27 +0200

    Improve image layout on mobile.

--- src/theme/BlogPostItems/index.tsx
+++ src/theme/BlogPostItems/index.tsx
@@ -32,22 +32,22 @@ export default function BlogPostItems({
   const docusaurusConfigPostsPerPageCount = result.siteConfig.presets[0][1].blog.postsPerPage;
 
   return (
-    <div className={styles.itemsListWrapper} style={{ marginTop: '2rem' }}>
+    <div className={styles.itemsListWrapper}>
       {/* <SurveyBroadcast /> */}
       {items.map(({content: BlogPostContent}) => {
         const blogPost = BlogPostContent.metadata;
 
         const date = DateTime.fromISO(blogPost.date).toLocaleString(DateTime.DATE_MED);
         return (
-          <div key={blogPost.permalink} className={styles.hoverHighlight} style={{ 'display': 'flex', borderRadius: '4px' }} onClick={() => navigateToLink(blogPost.permalink)}>
-            <div style={{ 'display': 'flex', minWidth: '200px', height: '210px' }}>
-              <div style={{ borderRadius: '4px 0 0 4px', width: '100%', backgroundImage: `url(${BlogPostContent.assets.image})`, backgroundSize: 'cover', backgroundPosition: 'center', aspectRatio: '1/1' }}>
+          <div key={blogPost.permalink} className={clsx(styles.hoverHighlight)} style={{ borderRadius: '10px' }} onClick={() => navigateToLink(blogPost.permalink)}>
+            <div style={{ 'display': 'flex' }} className={styles.imageWrapper}>
+              <div style={{ width: '100%', backgroundImage: `url(${BlogPostContent.assets.image})`, backgroundSize: 'cover', backgroundPosition: 'center', aspectRatio: '1/1' }}>
                 <div style={{ height: '100%', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
                   <FontAwesomeIcon icon="fa-regular fa-circle-play" className={styles.playIcon} size="4x" />
                 </div>
               </div>
             </div>
-            <div style={{ padding: '1rem 1rem 1rem 2rem' }}>
+            <div className={styles.episodeTitleBlock}>
               <h2 className={styles.title} style={{ marginBottom: '0.1rem' }}><a style={{ textDecoration: 'none' }} href={blogPost.permalink}>{blogPost.title}</a></h2>
               <small>{date}</small>
               <div className={styles.description}>{blogPost.description}</div>
@@ -57,7 +57,7 @@ export default function BlogPostItems({
       })}
       {items.length < docusaurusConfigPostsPerPageCount &&
         (<div style={{ minWidth: '200px', height: '200px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}> 
-          <div className={styles.hoverHighlight} style={{ height: '60px', minWidth: '300px', 'display': 'flex', borderRadius: '4px' }} onClick={() => navigateToLink('https://www.youtube.com/playlist?list=PLJesql-aSfX4-ySQL2GKoG585gkp7HuCf')}>
+          <div className={styles.hoverHighlight} style={{ minWidth: '300px', borderRadius: '4px' }} onClick={() => navigateToLink('https://www.youtube.com/playlist?list=PLJesql-aSfX4-ySQL2GKoG585gkp7HuCf')}>
             <div style={{ padding: '1rem 1rem 1rem 2rem' }}>
               <span style={{ display: 'flex', alignItems: 'center' }}>
                 <h2 className={styles.title} style={{ marginBottom: '0.1rem', marginRight: '0.5rem' }}>See More Episodes</h2> <FontAwesomeIcon icon="fa-solid fa-up-right-from-square" />
