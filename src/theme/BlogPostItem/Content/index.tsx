d7c2ca8a296d12ce273f43dcb8450e8e690b2c83
Warren Parad <5056218+wparad@users.noreply.github.com>
2025-12-04 08:50:05 +0200

    Simplify episode number validation logic
    Removed unnecessary error check for episode number based on date conditions.

--- src/theme/BlogPostItem/Content/index.tsx
+++ src/theme/BlogPostItem/Content/index.tsx
@@ -35,13 +35,6 @@ export default function BlogPostItemContent({
     transcriptLinkUrl = episodeNumber && `https://links.adventuresindevops.com/storage/episodes/${episodeNumber}-${episodeSlug}/transcript.txt`;
   }
 
-  if (!episodeNumber
-      && DateTime.fromISO('2025-08-01') < DateTime.fromJSDate(blogPost.frontMatter.date)
-      && DateTime.fromJSDate(blogPost.frontMatter.date) < DateTime.fromISO('2025-11-31')
-      && process.env.CI) {
-    throw Error(`No episode number for episode slug ${episodeSlug}, because usually the slug in the episode does not match what is saved in S3`);
-  }
-
   return (
     <div
       // This ID is used for the feed generation to locate the main content
